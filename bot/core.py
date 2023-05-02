3e9da45b82004d5d2a786e20fae80c0d66937106
Roman Ryzhkov <93090129+kpomak@users.noreply.github.com>
2023-05-03 02:39:36 +0300

    cron
    * add interval tasks
    
    * add mainloop

--- bot/core.py
+++ bot/core.py
@@ -2,24 +2,32 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(filename)s:%(lineno)d:%(levelname)s:[%(asctime)s] - %(name)s - %(message)s",
+)
 
-# Initialize bot and dispatcher
+# Initialize bot, storage and dispatcher
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+sheduler = AsyncIOScheduler()
 
 
-@dp.message_handler(commands=["start", "help"])
+@dp.message_handler(commands=["start"])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
+    This handler will be called when user sends `/start` command
     """
     await message.reply("Hi!\nI'm OrangeBot!\nPowered by aiogram.")
 
@@ -30,4 +38,5 @@ async def echo(message: types.Message):
 
 
 if __name__ == "__main__":
+    sheduler.start()
     executor.start_polling(dp, skip_updates=True)
