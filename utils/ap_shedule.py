045ff0ba4f38b343fceb9b0caf13c5eeefeaf5a8
kpomak <romka_ne@mail.ru>
2023-05-31 04:37:18 +0300

    test

--- utils/ap_shedule.py
+++ utils/ap_shedule.py
@@ -1,15 +1,15 @@
-import os
+# import os
 
 from aiogram import Bot
 
 from bot.handlers import db
 
 
-async def send_message_interval(bot: Bot):
-    await bot.send_message(
-        chat_id=os.getenv("ADMIN_ID"),
-        text="Это сообщение будет оптравляться с каким-то интервалом",
-    )
+# async def send_message_interval(bot: Bot):
+#     await bot.send_message(
+#         chat_id=os.getenv("ADMIN_ID"),
+#         text="Это сообщение будет оптравляться с каким-то интервалом",
+#     )
 
 
 async def notify(bot: Bot):
